6c8eeb8dcd6905ca6349c90afdea5ac6a3837d89
abentke <abentke@hs-mittweida.de>
2024-05-13 08:04:48 +0200

    nicht token ngrams und fixed csv to import

--- Aufgabe 3/ngram_counts_data_0.py
+++ Aufgabe 3/ngram_counts_data_0.py
@@ -5,7 +5,7 @@ from nltk.util import ngrams
 from collections import Counter
 
 # Download NLTK data
-#nltk.download('punkt')
+# nltk.download('punkt')
 
 # Function to generate n-grams from a list of tokens
 def generate_ngrams(tokens, n):
@@ -18,9 +18,9 @@ data_0 = pd.read_csv("Data Image 0.csv", sep="|", encoding="utf-16-LE")
 grouped_data = data_0.groupby("Sender Names")["Text"].apply(lambda x: ' '.join(map(str, x))).reset_index()
 
 # Create a file to write the n-grams
-output_file = "bigrams_data_0.csv"
+output_file = "trigrams_data_0.csv"
 with open(output_file, "w", encoding="utf-8") as file:
-    file.write("Sender,Ngram,Count\n")
+    file.write("Sender;Ngram;Count\n")
     
     # Iterate over each sender's messages and generate n-grams
     for index, row in grouped_data.iterrows():
@@ -31,14 +31,15 @@ with open(output_file, "w", encoding="utf-8") as file:
         tokens = word_tokenize(sender_messages)
         
         # Generate n-grams
-        n = 2  # Change n to the desired value for n-grams
+        n = 3  # Change n to the desired value for n-grams
         sender_ngrams = generate_ngrams(tokens, n)
+        #sender_ngrams = generate_ngrams(sender_messages, n)
         
         # Count the n-grams
         ngram_counts = Counter(sender_ngrams)
         
         # Write the sender name and n-gram counts to the file
         for ngram, count in ngram_counts.items():
-            file.write(f"{sender_name},{ngram},{count}\n")
+            file.write(f"{sender_name};{ngram};{count}\n")
 
 print(f"N-grams saved to {output_file}")
